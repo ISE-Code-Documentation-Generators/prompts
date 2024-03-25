32e165268fe1101e1af8de194547daf654c73603
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-03-25 20:23:16 +0330

    feat: init

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.0.1"
+DESCRIPTION = "To be added in the future"
+
+
+setuptools.setup(
+    name="ise_cdg_prompts",
+    version=VERSION,
+    author="Sepehr Kianian",
+    author_email="sepehr.kianian.r@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "ise_cdg_models @ git+https://github.com/ISE-Code-Documentation-Generators/models.git",
+        "ise_cdg_data @ git+https://github.com/ISE-Code-Documentation-Generators/data.git",
+        "ise_cdg_utility @ git+https://github.com/ISE-Code-Documentation-Generators/utility.git",
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+    ],
+)
