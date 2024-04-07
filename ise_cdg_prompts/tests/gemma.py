13f0175bf607a827bd9d7ad741455bb012b339ab
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-04-07 18:58:30 +0330

    refactor(gemma-sample-code): rename file

--- ise_cdg_prompts/tests/gemma.py
+++ ise_cdg_prompts/tests/gemma.py
@@ -7,14 +7,17 @@ class KossherTest(unittest.TestCase):
 
         self.io = JSON_IO("./")
 
+    def file_name_test_default(self) -> str:
+        return "gemma_results.json"
+
     def test_default(self):
         from ise_cdg_prompts.sample_codes.gemma import generate_response, prompt_list
 
         # f.write(outputs[0]["generated_text"])
         kir = prompt_list[9]
         # generate_response(kir)
-        # self.io.write({"jj": kir}, "generated_texttttt.txt")
-        kos = self.io.read("generated_texttttt.txt")["jj"]
+        # self.io.write({"jj": kir}, self.file_name_test_default())
+        kos = self.io.read(self.file_name_test_default())["jj"]
         # print("kiiiir")
         # print(kir)
         # print("koooos")
