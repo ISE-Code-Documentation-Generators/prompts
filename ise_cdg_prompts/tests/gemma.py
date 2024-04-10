a42dc312061a2d9118a4dac0e9f3b8c050c8f390
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-04-10 23:55:51 +0330

    test(gemma-sample-code): add test for ground truth

--- ise_cdg_prompts/tests/gemma.py
+++ ise_cdg_prompts/tests/gemma.py
@@ -11,18 +11,17 @@ class KossherTest(unittest.TestCase):
         return "gemma_results.json"
 
     def test_default(self):
-        from ise_cdg_prompts.sample_codes.gemma import generate_response, prompt_list
+        from ise_cdg_prompts.sample_codes.gemma import generate_response, prompt_list, grund_truth
 
-        # f.write(outputs[0]["generated_text"])
-        kir = prompt_list[9]
-        # generate_response(kir)
-        # self.io.write({"jj": kir}, self.file_name_test_default())
-        kos = self.io.read(self.file_name_test_default())["jj"]
+        # generate_response(prompt_list[9])
+        jj = {"prompts": prompt_list, "ground_truths": grund_truth}
+        self.io.write(jj, self.file_name_test_default())
+        kos = self.io.read(self.file_name_test_default())
         # print("kiiiir")
         # print(kir)
         # print("koooos")
         # print(kos)
-        self.assertEqual(kir, kos)
+        self.assertEqual(jj, kos)
 
 
 unittest.main(argv=[""], defaultTest="KossherTest", verbosity=2, exit=False)
