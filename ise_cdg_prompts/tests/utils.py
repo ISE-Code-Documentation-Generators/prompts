1aee148598678dd36e6805639bf7a05024e33dfa
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-04-13 15:37:35 +0330

    feat(tests): init utils class

---
+++ ise_cdg_prompts/tests/utils.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from unittest import TestCase
+    from ise_cdg_prompts.task import Task
+    from ise_cdg_prompts.prompt_generation_visitor.main import PromptGenerationVisitor
+
+
+from ise_cdg_prompts.utils.pipeline import Pipeline
+
+
+class AssertionUtils:
+    def __init__(self) -> None:
+        from ise_cdg_prompts.utils.custom_io import JSON_IO
+
+        self.io = JSON_IO("ise_cdg_prompts/tests")
+
+    def _assert_tasks_validity(
+        self,
+        unittest_module: "TestCase",
+        tasks: List["Task"],
+        prompt_generation_visitor: "PromptGenerationVisitor",
+        expected_tasks_file_name: str,
+    ):
+        test_output = (
+            Pipeline(tasks)
+            .to_map(lambda task: task.to_json(prompt_generation_visitor))
+            .to_list()
+        )
+        # self.io.write(test_output, expected_tasks_file_name)
+        expected_output = self.io.read(expected_tasks_file_name)
+        unittest_module.assertEqual(test_output, expected_output)
