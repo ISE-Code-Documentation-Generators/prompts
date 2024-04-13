c1d4a78cadc046653d10bc3adcf2d6997979af6f
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-04-13 15:24:37 +0330

    refactor(gemma-test): extract function

--- ise_cdg_prompts/tests/gemma.py
+++ ise_cdg_prompts/tests/gemma.py
@@ -4,26 +4,41 @@ import unittest
 
 from ise_cdg_prompts.tests.main import PromptsUnitTest
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ise_cdg_prompts.task import Task
+    from ise_cdg_prompts.sample.main import TaskSampler
+    from ise_cdg_prompts.prompt_generation_visitor.main import PromptGenerationVisitor
+
+from ise_cdg_prompts.utils.pipeline import Pipeline
+
 
 class GemmaUnitTests(PromptsUnitTest):
+    def _assert_tasks_validity(
+        self,
+        tasks: List["Task"],
+        prompt_generation_visitor: "PromptGenerationVisitor",
+        expected_tasks_file_name: str,
+    ):
+        prompt_list = (
+            Pipeline(tasks)
+            .to_map(lambda task: prompt_generation_visitor.visit_task(task))
+            .to_list()
+        )
+        ground_truths = (
+            Pipeline(tasks).to_map(lambda task: task.get_ground_truth()).to_list()
+        )
 
-    def file_name_test_default(self) -> str:
-        return "gemma_results.json"
-    
+        output = {"prompts": prompt_list, "ground_truths": ground_truths}
+        # self.io.write(output, expected_tasks_file_name)
+        expected_output = self.io.read(expected_tasks_file_name)
+        self.assertEqual(output, expected_output)
+
     def test_default(self):
-        from typing import TYPE_CHECKING
-
-        if TYPE_CHECKING:
-            from ise_cdg_prompts.sample.main import TaskSampler
-            from ise_cdg_prompts.prompt_generation_visitor.main import PromptGenerationVisitor
-
-        from ise_cdg_prompts.utils.pipeline import Pipeline
-
-
         import random
 
         random.seed(0)
-
 
         from ise_cdg_prompts.alireza_dataset import AlirezaDataset
         from ise_cdg_prompts.prompt_generation_visitor.alireza import (
@@ -37,22 +52,18 @@ class GemmaUnitTests(PromptsUnitTest):
             shot_size=4,
             sample_size=10,
         )
-        prompt_generation_visitor: "PromptGenerationVisitor" = AlirezaPromptGenerationVisitor()
-
-
-        tasks = task_sampler.generate_samples()
-        prompt_list = (
-            Pipeline(tasks)
-            .to_map(lambda task: prompt_generation_visitor.visit_task(task))
-            .to_list()
+        prompt_generation_visitor: "PromptGenerationVisitor" = (
+            AlirezaPromptGenerationVisitor()
         )
-        ground_truths = Pipeline(tasks).to_map(lambda task: task.get_ground_truth()).to_list()
 
-        output = {"prompts": prompt_list, "ground_truths": ground_truths}
-        # self.io.write(output, self.file_name_test_default())
-        expected_output = self.io.read(self.file_name_test_default())
-        self.assertEqual(output, expected_output)
+        self._assert_tasks_validity(
+            tasks=task_sampler.generate_samples(),
+            prompt_generation_visitor=prompt_generation_visitor,
+            expected_tasks_file_name="gemma_results.json",
+        )
 
 
 if __name__ == "__main__":
-    unittest.main(argv=[""], defaultTest=GemmaUnitTests.__name__, verbosity=2, exit=False)
+    unittest.main(
+        argv=[""], defaultTest=GemmaUnitTests.__name__, verbosity=2, exit=False
+    )
