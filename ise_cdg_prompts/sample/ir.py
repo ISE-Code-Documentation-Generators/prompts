65e56fd5b99cc2631f0b8339f3b28e52ca24eae3
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-04-23 18:03:07 +0330

    fix: fix IRBasedSampler

--- ise_cdg_prompts/sample/ir.py
+++ ise_cdg_prompts/sample/ir.py
@@ -7,14 +7,14 @@ from ise_cdg_prompts.task import Task
 from ise_cdg_data.information_retrieval import CodeRetrieval, load_semantical_ir_model
 
 if TYPE_CHECKING:
-    from ise_cdg_data.dataset import Md4DefDatasetInterface
+    from ise_cdg_data.dataset import DatasetFilterByIndexRaw
 
 
 class IRBasedTaskSampler(TaskSampler):
     def __init__(
         self,
-        train_dataset: "Md4DefDatasetInterface",
-        test_dataset: "Md4DefDatasetInterface",
+        train_dataset: "DatasetFilterByIndexRaw",
+        test_dataset: "DatasetFilterByIndexRaw",
         shot_size: int,
         device,
     ) -> None:
@@ -23,17 +23,23 @@ class IRBasedTaskSampler(TaskSampler):
         self.test_dataset = test_dataset
         self.__shot_size = shot_size
         self.ir: "CodeRetrieval" = load_semantical_ir_model(
-            self.train_dataset.source.to_list(),
+            [
+                self.train_dataset.get_raw_item("source", i)
+                for i in range(len(self.train_dataset))
+            ],
             device,
-            self.train_dataset.df["embeding"].to_list(),
+            [
+                self.train_dataset.get_raw_item("embeding", i)
+                for i in range(len(self.train_dataset))
+            ],
         )
 
     def generate_samples(self) -> List[Task]:
         tasks: List[Task] = []
-        for index in range(len(self.test_dataset.source)):
+        for index in range(len(self.test_dataset)):
             query_code, query_markdown = (
-                self.test_dataset.source.iloc[index],
-                self.test_dataset.md.iloc[index],
+                self.test_dataset.get_raw_item("source", index),
+                self.test_dataset.get_raw_item("markdown", index),
             )
             results = self.ir.get_similar(query_code)[: self.__shot_size]
             question = CodeMarkdown(query_code, query_markdown)
@@ -41,8 +47,8 @@ class IRBasedTaskSampler(TaskSampler):
             for result in results:
                 ind = result["corpus_id"]
                 template_code, template_markdown = (
-                    self.train_dataset.source.iloc[ind],
-                    self.train_dataset.markdown.iloc[ind],
+                    self.train_dataset.get_raw_item("source", ind),
+                    self.train_dataset.get_raw_item("markdown", ind),
                 )
                 templates.append(CodeMarkdown(template_code, template_markdown))
             tasks.append(Task(question, templates))
