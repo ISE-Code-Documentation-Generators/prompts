0aa80109074c992143e3386afe0b8ba077c739fe
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-05-18 15:22:33 +0330

    feat: init feature

--- ise_cdg_prompts/utils/metrics.py
+++ ise_cdg_prompts/utils/metrics.py
@@ -1,43 +1,47 @@
-from typing import List, Dict
+from typing import List, Dict, Optional, TYPE_CHECKING
 from ise_cdg_utility.metrics.enums import CodeMetric
-from ise_cdg_utility.metrics.interface import NLPMetricInterface, VectorizedNLPMetric
-from ise_cdg_data.tokenize import get_source_and_markdown_tokenizers
+from ise_cdg_utility.metrics.interface import NLPMetricInterface
 from ise_cdg_data.summarize import get_sumy_summarizer
 
+if TYPE_CHECKING:
+    from ise_cdg_utility.metrics.interface.src import NLPMetricTorchmetrics
 
-class LLMNLPMetricAdaptor(VectorizedNLPMetric):
-    def __init__(self, p95_len: int, nlp_metric: NLPMetricInterface) -> None:
+
+class LLMNLPMetricAdaptor(NLPMetricInterface):
+    def __init__(
+        self, p95_len: Optional[int], nlp_metric: "NLPMetricTorchmetrics"
+    ) -> None:
         self.p95_len = p95_len
         self.nlp_metric = nlp_metric
-        _, self.md_tokenizer = get_source_and_markdown_tokenizers(cleanse_markdown=False)
         self.summarizer = get_sumy_summarizer(2)
 
     def set_references(self, references: List[str]) -> None:
-        new_references = []
-        for i in range(len(references)):
-            ref = references[i]
-            reference = self.md_tokenizer(ref)
-            new_references.append([reference])
-        self.nlp_metric.set_references(new_references)
+        self.nlp_metric.set_references(references)
 
     def __call__(self, candidates: List[str]):
         _summarized = 0
         new_candidates = []
         for i in range(len(candidates)):
             candid = candidates[i]
-            if len(candid) > self.p95_len:
+            if self.p95_len is not None and len(candid) > self.p95_len:
                 candid = self.summarizer(candid)
                 _summarized += 1
-            candidate = self.md_tokenizer(candid)
-            new_candidates.append(candidate)
-        print(f"Warning: summarized {_summarized} candidates for {self.nlp_metric.__class__.__name__}!")
+            new_candidates.append(candid)
+        print(
+            f"Warning: summarized {_summarized} candidates for {self.nlp_metric.__class__.__name__}!"
+        )
         return self.nlp_metric(new_candidates)
 
 
-def get_metrics() -> Dict[CodeMetric, LLMNLPMetricAdaptor]:
-    from ise_cdg_utility.metrics.src import NLPMetricRangedBLEU, NLPMetricROUGE, NLPMetricBERT
+def get_metrics(p95_md_len: Optional[int]) -> Dict[CodeMetric, LLMNLPMetricAdaptor]:
+    from ise_cdg_utility.metrics.src import (
+        NLPMetricRangedBLEU,
+        NLPMetricROUGE,
+        NLPMetricBERT,
+    )
+
     def get_metric(metric):
-        return LLMNLPMetricAdaptor(1200, metric)
+        return LLMNLPMetricAdaptor(p95_md_len, metric)
 
     return {
         CodeMetric.BLEU: get_metric(NLPMetricRangedBLEU()),
