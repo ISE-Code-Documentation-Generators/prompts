24be33ea102eedb41f24f50203c9142784df07bb
Alireza Asadi <alireza.asadi@cafebazaar.ir>
2024-05-21 02:16:32 +0330

    Fix prompt for code metrics

--- ise_cdg_prompts/prompt_generation_visitor/hamed.py
+++ ise_cdg_prompts/prompt_generation_visitor/hamed.py
@@ -21,7 +21,8 @@ class HamedPromptGenerator(PromptGenerationVisitor):
     def visit_task(self, task: "TaskMetrics") -> str:
         metrics_string = self._get_metric_string(task.question.code)
         return (
-            "You are an expert Python programmer. Look at these methods, their code metrics, and their summaries:\n"
+            "Suppose you are an expert Python programmer. Here is the meaning of each code metric used later: LOC means Lines of Code, BLC means Number of Blank Lines of Code, UDF means Number of User-Defined Functions, I means Number of Imports, EH means Number of error handlings, ALLC means Average Line Length of Code, NDD means Number of Visualization Data Types, NEC means Number of Executed Cells, S means Number of Statements, P means Number of Parameters, KLCID means Kind of Line of Code Identifier Density, NBD means Nested Block Depth, OPRND means Number of Operands, OPRATOR means Number of Operators, UOPRND means Number of Unique Operands, UOPRATOR means Number of Unique Operators, ID means Number of Identifiers, ALID means Average Length of Identifiers, MLID means Max Length of Identifiers, CyC means Cyclomatic Complexity, EAP means External API Popularity, LOCom means Lines of Comments, CW means Number of Comment Words.\n"
+            + "Look at these methods, their code metrics, and their summaries:\n"
             + self.visit_templates(task.templates)
             + "\nNow given this code and its code metrics, please give me the summary of the code:\n"
             + f"\n#Code\n{task.question.code}\n#Code Metrics\n{metrics_string}"
