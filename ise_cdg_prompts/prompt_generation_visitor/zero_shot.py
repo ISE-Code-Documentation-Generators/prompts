b8ec096622c6f79b0f2735714e7861ad65ef9eb7
Alireza Asadi <alireza.asadi@cafebazaar.ir>
2024-05-25 15:59:44 +0330

    Fix zero shot prompts

--- ise_cdg_prompts/prompt_generation_visitor/zero_shot.py
+++ ise_cdg_prompts/prompt_generation_visitor/zero_shot.py
@@ -11,9 +11,10 @@ class ZeroShotPromptGenerator(PromptGenerationVisitor):
         sample_code = "def _combine_transfers(self, result):\ntransfers = {}\nfor reaction_id, c1, c2, form in result:\nkey = reaction_id, c1, c2\ncombined_form = transfers.setdefault(key, Formula())\ntransfers[key] = combined_form | form\n\nfor (reaction_id, c1, c2), form in iteritems(transfers):\nyield reaction_id, c1, c2, form"
         sample_summary = "Combine multiple pair transfers into one."
         return (
-            "Suppose you are an expert Python programmer."
-            + "\nLook at this python code and its summary:\n"
-            + f"\n#Code:\n{sample_code}\n#Summary:\n{sample_summary}\n"
-            + "\nNow given this code, please give me the summary of the code\n"
-            + f"\n#Code:\n{task.question.code}\n"
+            # "Suppose you are an expert Python programmer."
+            # + "\nLook at this python code and its summary:\n"
+            # + f"\n#Code:\n{sample_code}\n#Summary:\n{sample_summary}\n"
+            # + "\nNow given this code, please give me the summary of the code\n"
+            "Suppose you are an expert Python programmer. Give me the summary of the code from the corresponding code:\n"
+            + f"\nCode 1: ```\n{task.question.code}\n```\nSummary 1:"
         )
