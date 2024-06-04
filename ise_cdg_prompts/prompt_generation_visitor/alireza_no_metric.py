8943fcba72dc645167c5cf365ce23ac037a6067f
Alireza Asadi <alireza.asadi@cafebazaar.ir>
2024-06-04 19:30:49 +0330

    Add new better prompt generators

---
+++ ise_cdg_prompts/prompt_generation_visitor/alireza_no_metric.py
@@ -0,0 +1,28 @@
+from typing import TYPE_CHECKING, List
+from ise_cdg_prompts.prompt_generation_visitor.main import PromptGenerationVisitor
+
+if TYPE_CHECKING:
+    from ise_cdg_prompts.dataset import CodeMarkdown, CodeMarkdownMetrics
+    from ise_cdg_prompts.task import Task
+
+
+class AlirezaNoMetricPromptGenerator(PromptGenerationVisitor):
+    def visit_task(self, task: "Task") -> str:
+        return (
+            "Give me the summary of the code from the corresponding code. Codes are delimited by triple backquotes and summaries are delimited by triple quotes:\n"
+            + self.visit_templates(task.templates)
+            + f'\nCode {len(task.templates) + 1}: ```{task.question.code}```\nSummary {len(task.templates) + 1}:'
+        )
+
+    def visit_templates(self, templates: List["CodeMarkdown"]) -> str:
+        return "".join(
+            [
+                self.visit_template(template, index)
+                for index, template in enumerate(templates)
+            ]
+        )
+
+    def visit_template(self, template: "CodeMarkdownMetrics", index: int) -> str:
+        code_prompt = f"Code {index+1}: ```{template.code}```\n"
+        summary_prompt = f'Summary {index+1}: """{template.markdown}"""\n'
+        return code_prompt + summary_prompt
